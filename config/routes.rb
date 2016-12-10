570866a56d7cae8a6fcb886c2933f493d3808879
Stuart Yang <smyang10@gmail.com>
2016-12-10 02:41:39 -0800

    Updated look for week 2 assignment

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   	get "signin" => 'devise/sessions#new'
   	delete "signout" => 'devise/sessions#destroy'
   	get "signup" => 'devise/registrations#new'
+  	#get "signup" => redirect("http://us.battle.net/hearthstone/en/"), :as => :signup
   end
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
