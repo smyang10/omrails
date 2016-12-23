8d79e7756ce3d8c47f0631263af85c9fa4da32ee
Stuart Yang <smyang10@gmail.com>
2016-12-22 21:58:01 -0800

    Added user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
+  get 'users/:username', to: "users#show", as: 'user'
+
   resources :items
   resources :decks
   resources :tweets
   ActiveAdmin.routes(self)
   devise_for :users
   as :user do
-  	get "signin" => 'devise/sessions#new'
-  	delete "signout" => 'devise/sessions#destroy'
-  	get "signup" => 'devise/registrations#new'
+  	get "signin", to: 'devise/sessions#new'
+  	delete "signout", to: 'devise/sessions#destroy'
+  	get "signup", to: 'devise/registrations#new'
   	#get "signup" => redirect("http://us.battle.net/hearthstone/en/"), :as => :signup
   end
   root 'pages#home'
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
