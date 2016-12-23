18867ef22c673d84003e021868434fc11021b190
Stuart Yang <smyang10@gmail.com>
2016-12-23 00:25:50 -0800

    Fixed bugs when not logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   #get 'users/:username', to: "users#show", as: 'user'
-
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get "signup", to: 'devise/registrations#new'
+    #get "signup" => redirect("http://us.battle.net/hearthstone/en/"), :as => :signup
+  end
   get 'feed', to: 'feed#show'
 
   resources :users, only: :show, param: :username do
@@ -14,14 +21,7 @@ Rails.application.routes.draw do
   resources :items
   resources :decks
   resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-  	get "signin", to: 'devise/sessions#new'
-  	delete "signout", to: 'devise/sessions#destroy'
-  	get "signup", to: 'devise/registrations#new'
-  	#get "signup" => redirect("http://us.battle.net/hearthstone/en/"), :as => :signup
-  end
+  
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
